162fd75b436944cd8f28f3e73ef9aa0b63ff7e4e
only1Migwi <charliemigwi@gmail.com>
2021-02-28 11:14:41 +0300

    set CCV validation with Cleave.js

--- main.js
+++ main.js
@@ -39,4 +39,9 @@ const cleaveCC = new Cleave ("#cardNumber", {
 const cleaveDate = new Cleave("#cardExpiry", {
     date : true,
     datePattern : ['m', 'y'],
-})
+});
+
+const cleaveCCV = new Cleave("#cardCcv", {
+    blocks : [3],
+
+});
